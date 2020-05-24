07eae6c37ef666d81aa46d6207755c7a64b2bb87
Konosuke Odajima <konsuke.nosuke@icloud.com>
2020-05-25 00:13:34 +0900

    あああ

---
+++ kennsakuhozonnTests/kennsakuhozonnTests.swift
@@ -0,0 +1,34 @@
+//
+//  kennsakuhozonnTests.swift
+//  kennsakuhozonnTests
+//
+//  Created by 小田島康之介 on 2020/02/22.
+//  Copyright © 2020 小田島康之介. All rights reserved.
+//
+
+import XCTest
+@testable import kennsakuhozonn
+
+class kennsakuhozonnTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
